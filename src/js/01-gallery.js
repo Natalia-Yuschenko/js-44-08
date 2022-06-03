190dd8690f8f659e822b6d5a9b230bf565b7acfe
Natalia-Yuschenko <nata.yuschenko91@gmail.com>
2022-06-03 13:52:14 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// console.log(galleryItems);
+let markupPage = '';
+
+const refs = {
+  galleryElement: document.querySelector('.gallery'),
+};
+
+galleryItems.forEach(({ preview, original, description }) => {
+  markupPage += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+});
+
+addMarkupPage(markupPage);
+
+function addMarkupPage(markup) {
+  refs.galleryElement.insertAdjacentHTML('beforeend', markup);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+const storage = {
+  addItem(key, value) {
+    const result = JSON.stringify(value);
+    localStorage.setItem(key, result);
+  },
+
+  getItem(key) {
+    try {
+      const payloadOn = localStorage.getItem(key);
+      return JSON.parse(payloadOn);
+    } catch (error) {
+      console.error('Error parse');
+    }
+  },
+};
+
+iframePlayer.on(
+  'timeupdate',
+  throttle(function (data) {
+    storage.addItem('videoplayer-current-time', data.seconds);
+  }, 1000),
+);
+addEventListener('DOMContentLoaded', onPageInit);
+
+function onPageInit() {
+  const seconds = localStorage.getItem('videoplayer-current-time');
+  iframePlayer.setCurrentTime(seconds);
+}
